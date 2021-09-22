9b660fbe8bcf8e05557ff03a8935ccb2d198b9f4
HuanBlue <65179627+danHun26@users.noreply.github.com>
2021-09-22 14:06:18 +0700

    thêm database Linq

--- MailServer/Properties/Settings.Designer.cs
+++ MailServer/Properties/Settings.Designer.cs
@@ -8,21 +8,29 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-
-namespace MailServer.Properties
-{
+namespace MailServer.Properties {
+    
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
-    {
-
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.10.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-
-        public static Settings Default
-        {
-            get
-            {
+        
+        public static Settings Default {
+            get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=DESKTOP-7B4CM57\\MSSQLSERVER2;Initial Catalog=XAY_DUNG_MAIL_SERVER;Int" +
+            "egrated Security=True")]
+        public string XAY_DUNG_MAIL_SERVERConnectionString {
+            get {
+                return ((string)(this["XAY_DUNG_MAIL_SERVERConnectionString"]));
             }
         }
     }
