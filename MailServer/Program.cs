08b595c089fb2cbae025a464e4eeaab3b3be889f
HuanBlue <65179627+danHun26@users.noreply.github.com>
2021-09-22 20:55:35 +0700

    ...

--- MailServer/Program.cs
+++ MailServer/Program.cs
@@ -16,7 +16,7 @@ namespace MailServer
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new fGuiMail());
+            Application.Run(new fDangNhap());
         }
     }
 }
