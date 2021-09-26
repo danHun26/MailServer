97195d42c3281e725fdfcd397461e4af06aed02f
HuanBlue <65179627+danHun26@users.noreply.github.com>
2021-09-27 00:34:03 +0700

    làm lại form gửi mail

---
+++ MailServer/SendMail.Designer.cs
@@ -0,0 +1,186 @@
+﻿
+namespace MailServer
+{
+    partial class SendMail
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mailToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mởThưNhápToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lưuThưToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.thoátToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.chữĐậmToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.chữNghiênToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.chữGạchChânToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.xóaNộiDungToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tạoThưMớiToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.gửiMailToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(12, 30);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(776, 100);
+            this.panel1.TabIndex = 0;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.editToolStripMenuItem,
+            this.mailToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mởThưNhápToolStripMenuItem,
+            this.lưuThưToolStripMenuItem,
+            this.gửiMailToolStripMenuItem,
+            this.thoátToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.chữĐậmToolStripMenuItem,
+            this.chữNghiênToolStripMenuItem,
+            this.chữGạchChânToolStripMenuItem});
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(39, 20);
+            this.editToolStripMenuItem.Text = "Edit";
+            // 
+            // mailToolStripMenuItem
+            // 
+            this.mailToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.xóaNộiDungToolStripMenuItem,
+            this.tạoThưMớiToolStripMenuItem});
+            this.mailToolStripMenuItem.Name = "mailToolStripMenuItem";
+            this.mailToolStripMenuItem.Size = new System.Drawing.Size(42, 20);
+            this.mailToolStripMenuItem.Text = "Mail";
+            // 
+            // mởThưNhápToolStripMenuItem
+            // 
+            this.mởThưNhápToolStripMenuItem.Name = "mởThưNhápToolStripMenuItem";
+            this.mởThưNhápToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.mởThưNhápToolStripMenuItem.Text = "Mở thư nháp";
+            // 
+            // lưuThưToolStripMenuItem
+            // 
+            this.lưuThưToolStripMenuItem.Name = "lưuThưToolStripMenuItem";
+            this.lưuThưToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.lưuThưToolStripMenuItem.Text = "Lưu thư";
+            // 
+            // thoátToolStripMenuItem
+            // 
+            this.thoátToolStripMenuItem.Name = "thoátToolStripMenuItem";
+            this.thoátToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.thoátToolStripMenuItem.Text = "Thoát";
+            // 
+            // chữĐậmToolStripMenuItem
+            // 
+            this.chữĐậmToolStripMenuItem.Name = "chữĐậmToolStripMenuItem";
+            this.chữĐậmToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.chữĐậmToolStripMenuItem.Text = "Chữ đậm";
+            // 
+            // chữNghiênToolStripMenuItem
+            // 
+            this.chữNghiênToolStripMenuItem.Name = "chữNghiênToolStripMenuItem";
+            this.chữNghiênToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.chữNghiênToolStripMenuItem.Text = "Chữ nghiên";
+            // 
+            // chữGạchChânToolStripMenuItem
+            // 
+            this.chữGạchChânToolStripMenuItem.Name = "chữGạchChânToolStripMenuItem";
+            this.chữGạchChânToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.chữGạchChânToolStripMenuItem.Text = "Chữ gạch chân";
+            // 
+            // xóaNộiDungToolStripMenuItem
+            // 
+            this.xóaNộiDungToolStripMenuItem.Name = "xóaNộiDungToolStripMenuItem";
+            this.xóaNộiDungToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.xóaNộiDungToolStripMenuItem.Text = "Xóa nội dung";
+            // 
+            // tạoThưMớiToolStripMenuItem
+            // 
+            this.tạoThưMớiToolStripMenuItem.Name = "tạoThưMớiToolStripMenuItem";
+            this.tạoThưMớiToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.tạoThưMớiToolStripMenuItem.Text = "Tạo thư mới";
+            // 
+            // gửiMailToolStripMenuItem
+            // 
+            this.gửiMailToolStripMenuItem.Name = "gửiMailToolStripMenuItem";
+            this.gửiMailToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.gửiMailToolStripMenuItem.Text = "Gửi mail";
+            // 
+            // SendMail
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "SendMail";
+            this.Text = "SendMail";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem mởThưNhápToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem lưuThưToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem gửiMailToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem thoátToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem chữĐậmToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem chữNghiênToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem chữGạchChânToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem mailToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem xóaNộiDungToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tạoThưMớiToolStripMenuItem;
+    }
+}
